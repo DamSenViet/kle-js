5fc1a584f8960dba47e7c64b964368959e6f4dbf
Viet Tran <26016342+DamSenViet@users.noreply.github.com>
2021-08-14 21:49:20 -0700

    tests: added integrated outputs test

---
+++ tests/integrated/outputs.test.ts
@@ -0,0 +1,39 @@
+import fs from 'fs';
+import path from 'path';
+import Keyboard, { KeyboardJSON } from '../../src/Keyboard';
+
+const inputsPath: string = path.resolve(__dirname, '..', 'inputs');
+const outputsPath: string = path.resolve(__dirname, '..', 'outputs');
+
+fs.mkdirSync(outputsPath, { recursive: true });
+
+const filePaths: Array<string> = (() => {
+  try { return fs.readdirSync(inputsPath); }
+  catch { return []; }
+})()
+  .filter(filePath => filePath.endsWith('.json'))
+  .map(filePath => path.resolve(inputsPath, filePath));
+
+const inputs = filePaths.map(filePath => {
+  const filename = filePath.replace(/^.*[\\\/]/, '');
+  return {
+    filename,
+    inputPath: filePath,
+    outputPath: path.resolve(outputsPath, filename),
+  };
+});
+
+describe.each(inputs)('$outputPath', ({
+  inputPath,
+  outputPath,
+}) => {
+  it(`matches ${inputPath}`, () => {
+    const encoding = 'utf-8';
+    const inputFile: string = fs.readFileSync(inputPath, encoding);
+    const keyboardJSON: KeyboardJSON = JSON.parse(inputFile);
+    const keyboard: Keyboard  = Keyboard.fromJSON(keyboardJSON);
+    const outputFile: string = JSON.stringify(keyboard.toJSON(), null, '  ');
+    fs.writeFileSync(outputPath, outputFile, { encoding });
+    expect(outputFile).toBe(inputFile);
+  });
+});
