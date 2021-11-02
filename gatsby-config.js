6daac9c24d4e765418fc14973e5863281a72fecf
Viet Tran <26016342+DamSenViet@users.noreply.github.com>
2021-11-02 03:36:32 -0700

    docs: added typedoc

--- gatsby-config.js
+++ gatsby-config.js
@@ -20,6 +20,39 @@ module.exports = {
         docsPath: `src/docs`,
         repositoryUrl: `https://github.com/jpedroschmitz/rocketdocs`,
         baseDir: `examples/gatsby-theme-docs`,
+        withMdx: false, // using gatsby-plugin-mdx instead
+      },
+    },
+    {
+      resolve: `gatsby-plugin-mdx`,
+      options: {
+        gatsbyRemarkPlugins: [
+          {
+            resolve: 'gatsby-remark-typedoc-symbol-links',
+            options: {
+              basePath: '/api/',
+              linkTitleMessage(symbolPath, missing) {
+                return missing
+                  ? `Missing link to '${symbolPath}' docs. We will happily accept a PR to fix this! 🙏`
+                  : `View '${symbolPath}' in kle-js API docs.`;
+              }
+            },
+          },
+          // oriignal withMdx options
+          `gatsby-remark-autolink-headers`,
+          `gatsby-remark-embedder`,
+          {
+            resolve: `gatsby-remark-images`,
+            options: {
+              maxWidth: 960,
+              withWebp: true,
+              linkImagesToOriginal: false,
+            },
+          },
+          `gatsby-remark-responsive-iframe`,
+          `gatsby-remark-copy-linked-files`,
+        ],
+        plugins: [`gatsby-remark-autolink-headers`, `gatsby-remark-images`],
       },
     },
     {
@@ -48,5 +81,15 @@ module.exports = {
       },
     },
     `gatsby-plugin-offline`,
+    {
+      resolve: 'gatsby-source-typedoc',
+      options: {
+        src: [`${__dirname}/package/index.ts`],
+        typedoc: {
+          tsconfig: `${__dirname}/tsconfig.json`,
+          entryPoints: [`${__dirname}/package/index.ts`],
+        },
+      },
+    },
   ],
 };
