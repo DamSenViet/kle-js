51d992b6005d2ffa97f499fa887066ca0e150c26
Viet Tran <26016342+DamSenViet@users.noreply.github.com>
2021-11-13 01:10:00 -0800

    chore: setup github actions

---
+++ .github/workflows/deploy-docs.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+set -e
+
+cd "${GITHUB_WORKSPACE}"
+
+echo "DEPENDENCIES: INSTALLING..."
+npm i
+echo "DEPENDENCIES: COMPLETE"
+
+echo "TESTS: TESTING..."
+npm run test:esm
+echo "TESTS: COMPLETE"
+
+echo "BUILD: BUILDING..."
+npm run build:docs
+echo "BUILD: COMPLETE"
+
+# make new repo at build directory
+# have new repo overwrite-gh-pages
+cd public
+
+echo "GIT: CONFIGURING..."
+git init
+git config user.name "${GITHUB_ACTOR}"
+git config user.email "${GITHUB_ACTOR}@users.noreply.github.com"
+echo "GIT: CONFIGURED"
+
+echo "GIT: COMMITTING..."
+git add .
+git commit -m "Auto deploy from GitHub Actions"
+echo "GIT: COMITTED"
+
+echo "GIT: PUSHING..."
+git push -f "https://${ACCESS_TOKEN}@github.com/${GITHUB_REPOSITORY}.git" master:gh-pages
+echo "GIT: PUSHED"
