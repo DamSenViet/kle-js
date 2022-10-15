b46b92f9efdcf59cec0f3b94d0058b6d82ba0ac7
Viet <26016342+DamSenViet@users.noreply.github.com>
2022-10-15 05:04:27 -0700

    core: export schema types

--- package/index.ts
+++ package/index.ts
@@ -4,4 +4,12 @@ export { default as Background } from './Background';
 export { default as Key } from './Key';
 export { default as Switch } from './Switch';
 export { default as Label } from './Label';
+export {
+  KeyLabelsJSON,
+  KeyChangesJSON,
+  ClusterJSON,
+  BackgroundJSON,
+  MetadataJSON,
+  KeyboardJSON,
+} from "./schema-types"
 export * from './utils';
